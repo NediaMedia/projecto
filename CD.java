9eeaa3e2113af9fb8ec2d9bd7361a00faf2485f3
NediaMedia <68828536+NediaMedia@users.noreply.github.com>
2023-07-17 22:09:27 -0400

    Add files via upload

---
+++ CD.java
@@ -0,0 +1,18 @@
+package bookstore;
+
+public class CD extends Product {
+    private String artist;
+
+    public CD(String name, String artist, double price, int stock) {
+        super(name, price, stock);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
---
+++ DVD.java
@@ -0,0 +1,18 @@
+package bookstore;
+
+public class DVD extends Product {
+    private String category;
+
+    public DVD(String name, String category, double price, int stock) {
+        super(name, price, stock);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
